990db7fd2e032116cad5990d3144f9995aba8dd1
Jacques Gregoire <jacques471@gmail.com>
2020-08-30 17:55:14 -0400

    Created Jacques Gregoire_PE-4

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int num1 = 59;
+            // int num2 = 4;
+            string myName = "Jacques ";
+
+            myName = myName + "Gregoire";
+
+            Console.WriteLine(myName);
+                
+                string name = Console.ReadLine();          
+                
+                Console.WriteLine("50 plus 25 is " + 50 + 25);
+
+            //Console.Write("Hi there");
+            //Console.Write("David");
+                }
+    }
+}
---
+++ Jacqes Gregoire_PE-4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jacqes_Gregoire_PE_4
+{
+    class Program
+    {
+        //Author: Jacques Gregoire
+        //Purpose: To ask the user for two numbers until 
+        //they input only one that is greater than 10
+        //
+        static void Main(string[] args)
+        {
+            int var1 = 0;
+            int var2 = 0;
+            bool greater = true;
+
+            while (greater == true)
+            {
+                Console.WriteLine("Please enter a number...");
+
+                var1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter another number...");
+
+                var2 = Convert.ToInt32(Console.ReadLine());
+
+                greater =! (var1 > 10) ^ (var2 > 10);
+
+                if(greater == true)
+                {
+                    Console.WriteLine("Numbers rejected! Enter at least one integer less than 10.");
+                }
+                else
+                {
+                    Console.WriteLine("Number accepted, disarming self-descruct. Have a nice day!");
+                }
+
+            }
+            
+            
+        }
+    }
+}
