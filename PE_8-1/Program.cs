c69c02c474ec021c3070f0445f078ebbeef403e6
Jacques Gregoire <jacques471@gmail.com>
2020-09-07 20:36:31 -0400

    Created PE_8_2.

---
+++ PE_8-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PE_8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;            
+            double y;
+            double z;
+            int i = 0;
+            int j = 0;
+            double[,,] bArray = new double[30, 40, 1200];
+
+                    
+
+            //z = Math.Pow(3 * y, 2) + (2 * x) - 1;  
+
+            for (x = -1; x <= 1; x += 0.1)
+            {
+                i++;
+                for (y = 1; y <= 4.1; y += 0.1)
+                {
+                    j++;
+                    z = Math.Pow(3 * y, 2) + (2 * x) - 1;
+
+                    bArray[i, j, 0] = x;
+                    bArray[i, j, 1] = y;
+                    bArray[i, j, 2] = z;
+
+                    Console.WriteLine(x);
+                    Console.WriteLine(y);
+                    Console.WriteLine(z);
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ PE_8-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //various varibles
+            char single = 'n';
+
+            //asks the user for a string
+            Console.WriteLine("please enter a string");
+            string input = Console.ReadLine();
+            char[] cWord = input.ToCharArray();
+            char[] dWord = new char[cWord.Length];
+            
+            //prints out the string in reverse.
+            for (int i = 0; i < cWord.Length; i++)
+            {
+                single = cWord[(cWord.Length -1) - i];
+                //Console.WriteLine(cWord.Length);
+                dWord[i] = single;
+                Console.Write(single);                
+            }
+            Console.WriteLine("");
+            
+            
+        }
+    }
+}
