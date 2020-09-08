8f57c2173c1a92adfe7ae409e86a518d3eb4153d
Jacques Gregoire <jacques471@gmail.com>
2020-09-07 21:31:03 -0400

    Created PE_8-4

---
+++ PE_8-4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_8_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //various arrays
+            Char[] splitter = { ' ' };
+            string[] replace;
+           
+            //prompts the user for a string
+            Console.WriteLine("Please enter a string");
+            string input = Console.ReadLine();           
+
+            //adds a "" around each word in the string
+            replace = input.Split(splitter);
+
+            foreach(string single in replace)
+            {
+                Console.Write("\"{0}\"", single);
+            }
+            Console.WriteLine("");
+
+        }
+    }
+}
