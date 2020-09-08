009b2d8b0e7c55a6ce042c48f8f3b3c0a2a2fbfc
Jacques Gregoire <jacques471@gmail.com>
2020-09-07 20:59:16 -0400

    Created PE_8-3

---
+++ PE_8-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_8_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompts the user for a sting
+            Console.WriteLine("please enter a string");            
+            string input = Console.ReadLine();
+
+            //replaces any 'no' with a 'yes'
+            input = input.ToLower();
+            string input2 = input.Replace("no", "yes");
+            
+            //prints out the new string
+            Console.WriteLine(input2);
+        }
+    }
+}
