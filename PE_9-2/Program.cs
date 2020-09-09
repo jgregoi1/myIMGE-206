f674f51660d55bc233af5f677c679740545db167
Jacques Gregoire <jacques471@gmail.com>
2020-09-09 16:52:31 -0400

    Created PE_9-2

---
+++ PE_9-2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_9_2
+{
+    class Program
+    {
+
+        delegate string stringReader(string s);
+        static void Main(string[] args)
+        {
+            //declares the delagate
+            Console.WriteLine("Please enter a string");
+
+            stringReader processReader;
+
+            processReader = new stringReader(Stringer);
+
+        }
+
+        public static string Stringer(string s)
+        {
+            s = Console.ReadLine();
+
+
+           return s;
+        }
+
+
+    }
+
+
+
+}
+
