89e2b8e8e5657f9efd3f9397edcb8ce0f455df14
Jacques Gregoire <jacques471@gmail.com>
2020-09-14 17:01:25 -0400

    Created Delegate function

---
+++ Delagate function/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delagate_function
+{
+    class Program
+    { delegate string readerFunction(string input);
+        static void Main(string[] args)
+        {
+
+            string nullHolder = null;
+
+            Console.WriteLine("please enter a string.");
+
+            readerFunction stringReader;
+
+            stringReader = new readerFunction(stringer);
+
+            nullHolder = stringReader(nullHolder);
+        }
+
+        static string stringer(string input)
+        {
+            input = Console.ReadLine();
+            return input;
+        }
+       
+    }
+   
+}
+
+
