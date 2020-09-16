a3a883aae2aeabefe5b360e6b4e47c8bd182b68b
Jacques Gregoire <jacques471@gmail.com>
2020-09-16 11:44:16 -0400

    Created Salary1.

---
+++ Salary1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sName;
+            double dSalary = 30000;
+            bool emp;
+
+            Console.WriteLine("Please enter your name");
+            sName = Console.ReadLine(); 
+            
+            emp = GiveRaise(sName.ToLower(), ref dSalary);
+
+            if (emp == true)
+            {
+                if (dSalary > 30000)
+                {
+                    Console.WriteLine("Boss-o-matic: Congratulations on the raise " + sName + "!");
+                    Console.WriteLine("Your salary is now $" + dSalary + ".");
+                }
+                else
+                {
+                    Console.WriteLine("Your salary is now $" + dSalary + " " + sName + ".");
+                    Console.WriteLine("Have a nice day! And rememeber, employees are our 9th most valuable asset.");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your salary is $" + dSalary + " " + sName + ".");
+                Console.WriteLine("Have a nice day! And rememeber, employees are our 9th most valuable asset.");
+
+            }
+        }
+
+
+        static bool GiveRaise(string name, ref double salary)
+        {
+            bool id = false;
+
+            if (name == "jacques gregoire")
+            {
+                salary = 19999.99;
+
+                id = true;
+            }
+
+            return id;
+
+
+        }
+
+
+    }
+}
