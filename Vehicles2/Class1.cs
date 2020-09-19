5ce7017144a2a880613a6f1fc817dd16445f9f31
Jacques Gregoire <jacques471@gmail.com>
2020-09-19 14:39:46 -0400

    created vehicles2

---
+++ Vehicles2/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+
+    public abstract class vehicle
+    {
+        public virtual void LoadPassenger()
+        {
+
+        }
+
+
+
+    }
+
+    public abstract class train : vehicle
+    {
+
+    }
+
+    public abstract class car : vehicle
+    {
+
+    }
+
+    public interface PassengerCarrier
+    {
+        //void loadPassenger();
+    }
+
+    public interface HeavyLoadCarrier
+    {
+
+    }
+
+    public class compact : car, PassengerCarrier
+    {
+
+    }
+
+    public class SUV : car, PassengerCarrier
+    {
+
+    }
+
+    public class pickup : car, HeavyLoadCarrier
+    {
+
+    }
+
+    public class PassangerTrain : train, PassengerCarrier
+    {
+
+    }
+
+    public class FreightTrain : train, HeavyLoadCarrier
+    {
+
+    }
+
+    public class _424DoubleBogey : train, HeavyLoadCarrier
+    {
+
+    }
+
+}
