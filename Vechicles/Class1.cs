1c38cbbcd0a429faf7d0367f0bdf3f61fd9cddaa
Jacques Gregoire <jacques471@gmail.com>
2020-09-19 14:30:35 -0400

    Created Vehicles library.

---
+++ Vechicles/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+
+    public abstract class vehicle
+    {
+        public virtual void LoadPassenger()
+        {
+
+        }
+
+        
+
+    }
+
+    public abstract class train : vehicle
+    {
+
+    }
+
+    public abstract class car : vehicle
+    {
+
+    }
+
+    public interface PassangerCarrier
+    {
+        void loadPassenger();
+    }
+
+    public interface HeavyLoadCarrier
+    {
+
+    }
+
+    public class compact : car, PassangerCarrier
+    {
+
+    }
+
+    public class SUV : car, PassangerCarrier
+    {
+
+    }
+
+    public class pickup : car, HeavyLoadCarrier
+    {
+
+    }
+
+    public class PassangerTrain : train, PassangerCarrier
+    {
+
+    }
+
+    public class FreightTrain : train, HeavyLoadCarrier
+    {
+
+    }
+
+    public class _424DoubleBogey : train, HeavyLoadCarrier
+    {
+
+    }
+
+}
