760e27776f1703f96361b464cce4efe3b33f54e0
Jacques Gregoire <jacques471@gmail.com>
2020-09-23 14:55:08 -0400

    Because the .dll file werent showing up, I created another courselib

---
+++ CourseLib2/Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLib2
+{    public class Course
+    {
+        public string courseCode;
+        public string Description;
+        public string TeacherEmail;
+        public Schedule schedule;
+
+        public Course(string courseCode, string Desription) { }
+
+    }
+}
