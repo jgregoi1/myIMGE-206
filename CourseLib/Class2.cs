e99e9593efa772ede88b303922ee8900d8e0904f
Jacques Gregoire <jacques471@gmail.com>
2020-09-23 13:56:11 -0400

    Created PE_12-2

---
+++ CourseLib/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLib
+{
+    public class Course
+    {
+        public string courseCode;
+        public string Description;
+        public string TeacherEmail;
+        public Schedule schedule;
+
+        public Course(string courseCode, string Desription) { }
+
+    }
+}
---
+++ CourseLib/Class3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLib
+{
+    public class Schedule
+    {
+        public DateTime startTime;
+        public DateTime endTime;
+
+        public List<DayOfWeek> daysOfWeek = new List<DayOfWeek>();
+    }
+}
