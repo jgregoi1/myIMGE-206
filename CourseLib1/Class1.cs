8129bed6ff8d6ff81dfbaaeafea866112a6e38f3
Jacques Gregoire <jacques471@gmail.com>
2020-09-23 14:41:08 -0400

    Created an alternate course library

---
+++ CourseLib1/Class1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLib1
+{
+    public class Courses
+    {
+        public Courses()
+        {
+            Course thisCourse;
+            Schedule thisSchedule;
+
+            Random rand = new Random();
+
+            // generate courses IGME-200 through IGME-299
+            for (int i = 200; i < 300; ++i)
+            {
+                // use constructor to create new course object with code and description
+                thisCourse = new Course(($"IGME-{i}"), ($"Description for IGME-{i}"));
+
+                // create a new Schedule object
+                thisSchedule = new Schedule();
+                for (int dow = 0; dow < 7; ++dow)
+                {
+                    // 50% chance of the class being on this day of week
+                    if (rand.Next(0, 2) == 1)
+                    {
+                        // add to the daysOfWeek list
+                        thisSchedule.daysOfWeek.Add((DayOfWeek)dow);
+
+                        // select random hour of day
+                        int nHour = rand.Next(0, 24);
+
+                        // set start and end times of minute duration
+                        // select fixed date to allow time calculations
+                        thisSchedule.startTime = new DateTime(1, 1, 1, nHour, 0, 0);
+                        thisSchedule.endTime = new DateTime(1, 1, 1, nHour, 50, 0);
+                    }
+                }
+
+                // set the schedule for this course
+                thisCourse.schedule = thisSchedule;
+
+                // add this course to the SortedList
+                this[thisCourse.courseCode] = thisCourse;
+            }
+        }
+
+
+
+        public SortedList<string, Course> sortedList = new SortedList<string, Course>();
+
+        public Course this[String courseCode]
+        {
+
+            get
+            {
+                Course returnVal;
+                try
+                {
+                    returnVal = (Course)sortedList[courseCode];
+                }
+                catch
+                {
+                    returnVal = null;
+                }
+                return returnVal;
+
+            }
+            set
+            {
+                try
+                {
+                    sortedList.Add(courseCode, value);
+                }
+                catch
+                {
+                    Console.WriteLine("A course with that code already exsists, pease try again");
+                }
+            }
+        }
+        public void Remove(string courseCode)
+        {
+            if (courseCode != null)
+            {
+                sortedList.Remove(courseCode);
+            }
+        }
+    }
+   
+    public class Course
+    {
+        public string courseCode;
+        public string Description;
+        public string TeacherEmail;
+        public Schedule schedule;
+
+        public Course(string courseCode, string Desription) { }
+
+    }
+
+    public class Schedule
+    {
+        public DateTime startTime;
+        public DateTime endTime;
+
+        public List<DayOfWeek> daysOfWeek = new List<DayOfWeek>();
+    }
+}
---
+++ Jacques_Gregoire people app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jacques_Gregoire_people_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
