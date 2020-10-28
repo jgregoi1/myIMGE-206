730d2ea69a3b20fe5bb0eec7db4494090c30c724
Jacques Gregoire <jacques471@gmail.com>
2020-10-28 17:17:29 -0400

    Finished Don'tDie

--- Don't Die/Program.cs
+++ Don't Die/Program.cs
@@ -11,7 +11,8 @@ namespace Don_t_Die
         north,
         south,
         east,
-        west
+        west,
+        none
     }
     class Program
     {
@@ -21,38 +22,51 @@ namespace Don_t_Die
         Edirection west = Edirection.west;
         static int[,] mCosts = new int[,]
         {
-            {0, 2, -1, -1 },  //a
-            {-1, 2, 3, -1},   //b
-            {2, 20, -1, -1}, //c
-            {2, 5, 4, 3},    //d
-            {-1, 3, -1, -1}, //e
-            {-1, -1, 1, -1}, //f
-            {0, 2, -1, -1},  //g
-            {-1, -1, -1, -1} //h
+            {0, 2, -1, -1, -1, -1, -1, -1},  //a
+            {-1, -1, 2, 3, -1, -1, -1, -1},   //b
+            {-1, 2, -1, -1, -1, -1, -1, 20}, //c
+            {-1, 3, 5, -1, 2, 4, -1, -1},    //d
+            {-1, -1, -1, -1, -1, 3, -1, -1}, //e
+            {-1, -1, -1, -1, -1, -1, 1, -1}, //f
+            {-1, -1, -1, -1, 0 , -1, -1, 2},  //g
+            {-1, -1, -1, -1, -1, -1, -1, -1} //h
         };
+                
 
         static Edirection[,] mDirection = new Edirection[,]
         {
-            {Edirection.north, Edirection.south, Edirection.east, Edirection.west}, //a
-            {Edirection.north, Edirection.south, Edirection.east, Edirection.west}, //b
-            {Edirection.north, Edirection.south, Edirection.east, Edirection.west}, //c
-            {Edirection.north, Edirection.south, Edirection.east, Edirection.west}, //d
-            {Edirection.north, Edirection.south, Edirection.east, Edirection.west}, //e
-            {Edirection.north, Edirection.south, Edirection.east, Edirection.west}, //f
-            {Edirection.north, Edirection.south, Edirection.east, Edirection.west}, //g
-            {Edirection.north, Edirection.south, Edirection.east, Edirection.west}  //h
+            {Edirection.none, Edirection.south, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none}, //a
+            {Edirection.none, Edirection.none, Edirection.none, Edirection.south, Edirection.east, Edirection.none,Edirection.none, Edirection.none}, //b
+            {Edirection.none, Edirection.north, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.south}, //c
+            {Edirection.none, Edirection.west, Edirection.south, Edirection.none, Edirection.north, Edirection.east, Edirection.none, Edirection.none}, //d
+            {Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.south, Edirection.none,Edirection.none}, //e
+            {Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.east, Edirection.none}, //f
+            {Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.north, Edirection.none, Edirection.none, Edirection.south}, //g
+            {Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none}  //h
         };
 
-        int[][] lCost = new int[][]
+        static int[][] lCost = new int[][]
         {
-            new int[] {0, 2},
-            new int[] {2, 3},
-            new int[] {2, 20},
-            new int[] {2, 5, 4, 3},
-            new int[] {3},
-            new int[] {1},
-            new int[] {0, 2},
-            new int[] { }
+            new int[] {0, 2, -1, -1, -1, -1, -1, -1},
+            new int[] {-1, -1, 2, 3, -1, -1, -1, -1},
+            new int[] {-1, 2, -1, -1, -1, -1, -1, 20},
+            new int[] {-1, 3, 5, -1, 2, 4, -1, -1},
+            new int[] {-1, -1, -1, -1, -1, 3, -1, -1},
+            new int[] {-1, -1, -1, -1, -1, -1, 1, -1},
+            new int[] {-1, -1, -1, -1, 0, -1, -1, 2},
+            new int[] {-1, -1, -1, -1, -1, -1, -1, -1}
+        };
+
+        static Edirection[][] lDirection = new Edirection[][]
+        {
+            new Edirection[] {Edirection.none, Edirection.south, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none},
+            new Edirection[] {Edirection.none, Edirection.none, Edirection.none, Edirection.south, Edirection.east, Edirection.none,Edirection.none, Edirection.none}, 
+            new Edirection[] {Edirection.none, Edirection.north, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.south}, 
+            new Edirection[] {Edirection.none, Edirection.west, Edirection.south, Edirection.none, Edirection.north, Edirection.east, Edirection.none, Edirection.none},
+            new Edirection[] {Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.south, Edirection.none,Edirection.none},
+            new Edirection[] {Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.east, Edirection.none},
+            new Edirection[] {Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.north, Edirection.none, Edirection.none, Edirection.south},
+            new Edirection[] {Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none, Edirection.none}  
         };
         static void Main(string[] args)
         {
